c1f48350142844e2ee55efafb429732d20d2a7a9
ycomiti <94963509+ycomiti@users.noreply.github.com>
2025-06-07 00:05:01 +0200

    Removed unused variables and imports

--- src/corsica/comiti/debloater/Main.java
+++ src/corsica/comiti/debloater/Main.java
@@ -1,26 +1,22 @@
 package corsica.comiti.debloater;
 
 import java.awt.Desktop;
-import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import corsica.comiti.debloater.adb.utils.Arguments;
 import corsica.comiti.debloater.adb.utils.Options;
 import corsica.comiti.debloater.frames.DebloaterFrame;
 import corsica.comiti.debloater.frames.DependsFrame;
-import corsica.comiti.debloater.utils.MessageBox;
 
 public class Main {
 
     private static final Main INSTANCE = new Main();
     private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     private final boolean isDebugging = false;
-
-    private File adbDirectory;
+    
     private Arguments arguments;
     private Options options;
 
